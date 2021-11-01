b0300f79a3664ce75579e5cae78a74dc1eecbf24
tparfeni <teodorparfeni@gmail.com>
2021-11-01 16:27:23 +0200

    Update

--- app/src/main/java/com/example/moviesapp/LogInActivity.kt
+++ app/src/main/java/com/example/moviesapp/LogInActivity.kt
@@ -1,8 +1,11 @@
 package com.example.moviesapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
@@ -15,26 +18,19 @@ class LogInActivity : AppCompatActivity() {
 
         val logInFragment = LogInFragment()
         val signUpFragment = SignUpFragment()
-        val toSignUp : Button = findViewById(R.id.toSignUp)
-        val toLogIn : Button = findViewById(R.id.toLogIn)
 
+        switchToFragment(logInFragment)
+    }
+    private fun switchToFragment(fragment: Fragment){
         supportFragmentManager.beginTransaction().apply {
-            replace(R.id.frameLogIn,logInFragment)
+            replace(R.id.frameLogIn,fragment)
             commit()
         }
+    }
 
-        toSignUp.setOnClickListener{
-            supportFragmentManager.beginTransaction().apply {
-                replace(R.id.frameLogIn,signUpFragment)
-                commit()
-            }
-        }
-
-        toLogIn.setOnClickListener{
-            supportFragmentManager.beginTransaction().apply {
-                replace(R.id.frameLogIn,logInFragment)
-                commit()
-            }
-        }
+    private fun openActivity(activity: Class<*>){
+        val intent = Intent(this,activity)
+        startActivity(intent)
+        finish()
     }
 }
--- app/src/main/java/com/example/moviesapp/LogInFragment.kt
+++ app/src/main/java/com/example/moviesapp/LogInFragment.kt
@@ -1,11 +1,47 @@
 package com.example.moviesapp
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class LogInFragment : Fragment(R.layout.login_fragment) {
+    override fun onAttach(context: Context) {
+        var sharedPrefs = context.getSharedPreferences("users",Context.MODE_PRIVATE)
+        var editor = sharedPrefs.edit()
+        super.onAttach(context)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.login_fragment,container,false)
+        var logInButton: Button = view.findViewById(R.id.logInButton)
+        var email: EditText = view.findViewById(R.id.email)
+        var password: EditText = view.findViewById(R.id.password)
+
+        logInButton.setOnClickListener{
+            Log.d("LogIn","DA")
+            if(email.text.toString() == "email" && password.text.toString() == "1234")
+                openActivity(MainActivity::class.java)
+        }
+
+
+        return view
+    }
+    private fun openActivity(nextActivity: Class<*>){
+        val intent = Intent(activity!!.application,nextActivity)
+        startActivity(intent)
+        activity!!.finish()
+    }
 
 }
