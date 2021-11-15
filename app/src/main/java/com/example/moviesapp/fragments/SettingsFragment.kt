2fffa69f80f6f454503c3b08f14caaeb302ebafb
TheGreatTeo <teodorparfeni@gmail.com>
2021-11-15 15:00:15 +0200

    Added the change language functionality inside SettingsFragment.

--- app/src/main/java/com/example/moviesapp/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/moviesapp/fragments/SettingsFragment.kt
@@ -1,6 +1,9 @@
 package com.example.moviesapp.fragments
 
 import android.app.Activity
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.res.Configuration
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -9,6 +12,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat.recreate
 import androidx.recyclerview.widget.RecyclerView
 import com.example.moviesapp.CoroutinesHomework
 import com.example.moviesapp.LogInActivity
@@ -19,6 +23,7 @@ import com.example.moviesapp.data.GenreItem
 import com.google.firebase.auth.FirebaseAuth
 import org.json.JSONArray
 import java.io.IOException
+import java.util.*
 
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
 
@@ -30,8 +35,10 @@ class SettingsFragment : Fragment(R.layout.fragment_settings) {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_settings, container, false)
 
+        loadLanguage()
         val signOutButton: Button = view.findViewById(R.id.signOut)
         val toCoroutine: Button = view.findViewById(R.id.toCoroutine)
+        val changeLanguage: Button = view.findViewById(R.id.changeLanguage)
 
         signOutButton.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
@@ -43,6 +50,39 @@ class SettingsFragment : Fragment(R.layout.fragment_settings) {
         toCoroutine.setOnClickListener {
             activityOpener.openActivity(requireContext().applicationContext,CoroutinesHomework::class.java)
         }
+
+        changeLanguage.setOnClickListener {
+            val languages = arrayOf("English","Romanian")
+            val alertDialog = AlertDialog.Builder(requireActivity())
+            alertDialog.setTitle(R.string.changeLanguageTitle)
+            alertDialog.setSingleChoiceItems(languages,-1,DialogInterface.OnClickListener { dialog, which ->
+                if(which == 0) {
+                    setLanguage("en")
+                    recreate(requireActivity())
+                }
+                if(which == 1) {
+                    setLanguage("ro")
+                    recreate(requireActivity())
+                }
+                dialog.dismiss()
+            })
+            alertDialog.create().show()
+        }
+
+
         return view
+    }
+
+    fun setLanguage(language: String){
+        var locales = Locale(language)
+        Locale.setDefault(locales)
+        var config = Configuration()
+        config.locale = locales
+        requireContext().resources.updateConfiguration(config,requireContext().resources.displayMetrics)
+        sharedPrefsHandler.setLanguage(requireContext(),language)
+    }
+
+    fun loadLanguage(){
+        setLanguage(sharedPrefsHandler.getLanguage(requireContext()))
     }
 }
