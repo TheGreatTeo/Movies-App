4a97f49858a77f8b87f9119890a4d5609e4749dc
TheGreatTeo <teodorparfeni@gmail.com>
2021-11-17 15:55:32 +0200

    Created a permission request for COARSE_LOCATION for homework.

--- app/src/main/java/com/example/moviesapp/Activities/LogInActivity.kt
+++ app/src/main/java/com/example/moviesapp/Activities/LogInActivity.kt
@@ -1,11 +1,20 @@
 package com.example.moviesapp.Activities
 
+import android.Manifest
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.moviesapp.controller.callback
 import com.example.moviesapp.fragments.LogInActivityFragments.LogInFragment
 import com.example.moviesapp.fragments.LogInActivityFragments.SignUpFragment
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.moviesapp.R
 
@@ -14,11 +23,19 @@ class LogInActivity : AppCompatActivity(), callback {
 
     var signUpFragment = SignUpFragment()
     var logInFragment = LogInFragment()
+    val LOCATION_PERMISSION_CODE  = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_New)
         setContentView(R.layout.activity_log_in)
+
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            Log.d("Permission","Is granted")
+        }
+        else{
+            requestLocationPermission()
+        }
 
         if(savedInstanceState == null)
             switchToLogIn()
@@ -78,6 +95,32 @@ class LogInActivity : AppCompatActivity(), callback {
         val fragments = supportFragmentManager.fragments
         supportFragmentManager.putFragment(outState,"fragment",fragments.get(fragments.size-1))
         super.onSaveInstanceState(outState)
+    }
+
+    fun requestLocationPermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_COARSE_LOCATION)){
+            val builder = AlertDialog.Builder(this)
+                    .setTitle("Location permission")
+                    .setMessage("This permission is needed!")
+                    .setPositiveButton("Ok",DialogInterface.OnClickListener { dialog, which ->
+                        ActivityCompat.requestPermissions(this,arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),LOCATION_PERMISSION_CODE)
+                    })
+                    .setNegativeButton("Cancel",DialogInterface.OnClickListener { dialog, which ->  dialog.dismiss() })
+                    .create().show()
+        }else{
+            ActivityCompat.requestPermissions(this,arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),LOCATION_PERMISSION_CODE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == LOCATION_PERMISSION_CODE){
+            if(grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this,"Permission is granted!",Toast.LENGTH_LONG)
+            }else{
+                Toast.makeText(this,"Permission denied!",Toast.LENGTH_LONG)
+            }
+        }
     }
 
 }
