eb11de9dc195b7982c8bd5f1c2690fb87d64f9fc
TheGreatTeo <teodorparfeni@gmail.com>
2021-11-22 16:19:15 +0200

    Modified the HomeFragment and added a welcome message with a user icon and the ViewPager for the Popular movies.

--- app/src/main/java/com/example/moviesapp/fragments/MainActivityFragments/HomeFragment.kt
+++ app/src/main/java/com/example/moviesapp/fragments/MainActivityFragments/HomeFragment.kt
@@ -9,8 +9,14 @@ import android.view.ViewGroup
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.lifecycle.lifecycleScope
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
 import com.example.moviesapp.API.tmdbAPI.TMDBInterface
+import com.example.moviesapp.API.tmdbAPI.TMDBJSON
 import com.example.moviesapp.R
+import com.example.moviesapp.controller.ViewPagerAdapter.Adapter
+import com.example.moviesapp.data.MovieItem
+import com.squareup.picasso.Picasso
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -18,32 +24,40 @@ import kotlinx.coroutines.withContext
 import retrofit2.awaitResponse
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
+    var movieList = ArrayList<MovieItem>()
+    var tmdbJSON = TMDBJSON(0,listOf<com.example.moviesapp.API.tmdbAPI.Result>())
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
-
         val progressBar: ProgressBar = view.findViewById(R.id.progressBar)
-        val textView: TextView = view.findViewById(R.id.textView)
+        var viewPager: ViewPager = view.findViewById(R.id.viewPager)
+        movieList = ArrayList<MovieItem>()
+        val adapter: Adapter = Adapter(movieList,this@HomeFragment.requireContext())
         progressBar.visibility = View.VISIBLE
-        textView.visibility = View.GONE
         lifecycleScope.launch() {
-            test()
+            getMovies()
+            viewPager.adapter = adapter
+            viewPager.setPadding(100,0,100,0)
+            viewPager.currentItem = movieList.size/2
             delay(200L)
             progressBar.visibility = View.GONE
-            textView.visibility = View.VISIBLE
         }
 
         return view
     }
 
-    suspend fun test(){
-        return withContext(Dispatchers.IO){
-            val tmdbInterface = TMDBInterface.create().getTopRatedMovies("9df4f48f58d1cb4702a2b4d936029e0d").awaitResponse()
-            if(tmdbInterface.isSuccessful){
-                val data = tmdbInterface.body()
-                Log.d("Raspuns",data.toString())
+    suspend fun getMovies() {
+        return withContext(Dispatchers.IO) {
+            val movies = TMDBInterface.create().getPopular("9df4f48f58d1cb4702a2b4d936029e0d").awaitResponse()
+            if (movies.isSuccessful) {
+                tmdbJSON = movies.body()!!
+                Log.d("Popular", tmdbJSON.toString())
+                for (i in tmdbJSON.results) {
+                    val movieItem = MovieItem(i.title,i.overview, Picasso.get().load("https://image.tmdb.org/t/p/w500" + i.poster_path),"",i.vote_average,i.poster_path)
+                    movieList.add(movieItem)
+                }
             }
         }
     }
